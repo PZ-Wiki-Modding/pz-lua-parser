5e13b54a1531e6f613a43760da9def77d3d5cb79
SimKDT <simon.cadet79@hotmail.fr>
2026-05-05 23:44:41 +0200

    distributions data

--- scripts/distributions.py
+++ scripts/distributions.py
@@ -1,5 +1,6 @@
-import json, yaml
+import json, yaml, lupa
 from pathlib import Path
+# from lupa import 
 
 from execute_lua import run_files
 
@@ -8,14 +9,18 @@ LUA_DIR = Path("/home/simon/.steam/debian-installation/steamapps/common/ProjectZ
 OUT = Path("out/distributions.json")
 OUT_ROOMS = Path("out/rooms.json")
 
-PROPERTIES_DATA = Path("data/distributions_room_properties.yaml")
+
+# parsing those properties is useless, they do nothing, see the properties file for the details
+# PROPERTIES_DATA = Path("data/ItemPickerRoom_properties.yaml")
+# with open(PROPERTIES_DATA) as f:
+#     PROPERTIES = yaml.safe_load(f)
+#     PROPERTIES = [
+#         v for v in PROPERTIES if not v.get('isUseless', False)
+#     ]
+
+PROPERTIES_DATA = Path("data/ItemPickerContainer_properties.yaml")
 with open(PROPERTIES_DATA) as f:
     PROPERTIES = yaml.safe_load(f)
-    PROPERTIES = {
-        k: v for k, v in PROPERTIES.items() if not v.get('isUseless', False)
-    }
-
-print(PROPERTIES)
 
 # load files in dependency order
 lua_files = [
@@ -41,13 +46,41 @@ dist = lua.globals()['Distributions']
 distrib_table = dist[1]
 
 rooms = []
-for room_name, data in distrib_table.items():
+rooms_distribution = {}
+for room_name, room_data in distrib_table.items():
     # PZ is a bitch so they store fucking procedural distributions alongside room definitions
     # literally in the same table, what the fucking fuck ???
-    if "rolls" in data:
+    if "rolls" in room_data:
         continue
 
+    # store room name
     rooms.append(room_name)
+    rooms_distribution[room_name] = {}
+
+    for container_name, container_data in room_data.items():
+        # if it isn't a table, then it's not a container def
+        if not lupa.lua_type(container_data) == 'table':
+            continue
+
+        # check that it contains 'procedural'
+        if not container_data['procedural']:
+            continue
+
+        procList = list(container_data['procList'].values())
+        procList = [dict(item) for item in procList]
+        for item in procList:
+            for prop in PROPERTIES:
+                val = item.get(prop['name'])
+                if val is None:
+                    continue
+                t = prop.get('type', {})
+                if t.get('main', None) == 'array':
+                    item[prop['name']] = val.split(t.get('separator', ';'))
+            print(item)
+
+        rooms_distribution[room_name][container_name] = procList
+
+    # parse container name
 
 
 # export rooms
@@ -55,3 +88,6 @@ with open(OUT_ROOMS, "w") as f:
     rooms.sort(key=str.lower)
     json.dump(rooms, f, indent=4)
 
+# export distributions
+with open(OUT, "w") as f:
+    json.dump(rooms_distribution, f, indent=4)
